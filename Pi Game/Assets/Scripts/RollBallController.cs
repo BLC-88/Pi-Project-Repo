5038f144b1dfe5ea9610c098b1c112301c3fd57e
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-09-28 22:30:24 +0100

    Added Jumping

--- Pi Game/Assets/Scripts/RollBallController.cs
+++ Pi Game/Assets/Scripts/RollBallController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class RollBallController : MonoBehaviour {
 
     [SerializeField] float torque = 250f;
+    [SerializeField] float jumpForce = 250f;
+    [SerializeField] LayerMask whatIsGround;
 
     Vector3 moveDir;
     CameraController cam;
@@ -16,12 +18,26 @@ public class RollBallController : MonoBehaviour {
         cam = FindObjectOfType<CameraController>();
     }
 
-    void FixedUpdate() {
+    void Update() {
         float hor = -Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
         moveDir = ((hor * cam.forward) + (ver * cam.right)).normalized;
 
+        if (CheckGrounded()) {
+            Debug.Log("grounded");
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+        }
+    }
+
+    void FixedUpdate() {
         rb.AddTorque(moveDir * torque * Time.deltaTime);
+    }
+
+    bool CheckGrounded() {
+        RaycastHit hit;
+        return Physics.SphereCast(transform.position, 0.5f, Vector3.down, out hit, 0.55f, whatIsGround);
     }
 }
