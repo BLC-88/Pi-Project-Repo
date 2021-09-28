8592d3433a921e6702358489a6af418b2dcd1818
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-09-28 22:09:06 +0100

    Added Camera Follow Script

--- Pi Game/Assets/Scripts/CameraController.cs
+++ Pi Game/Assets/Scripts/CameraController.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    [SerializeField] GameObject target;
+    [SerializeField] Vector3 offset = new Vector3(0, 3, -10);
+
     [HideInInspector] public Vector3 forward;
     [HideInInspector] public Vector3 right;
 
+    float lerpTime = 1f;
+    float currentLerpTime;
+    Vector3 startPos;
+    Vector3 endPos;
+
     void Start() {
-        
+        target = FindObjectOfType<RollBallController>().gameObject;
     }
 
     void Update() {
+        startPos = transform.position;
+        endPos = target.transform.position + offset;
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime > lerpTime) {
+            currentLerpTime = lerpTime;
+        }
+        float perc = currentLerpTime / lerpTime;
+        transform.position = Vector3.Lerp(startPos, endPos, perc);
+
         forward = Quaternion.Euler(0, transform.eulerAngles.y, 0) * Vector3.forward;
         right = Quaternion.Euler(0, transform.eulerAngles.y, 0) * Vector3.right;
     }
