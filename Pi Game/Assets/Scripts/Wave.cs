81c2a22f52abc817ae0eca210a2dce4c00b154a9
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-10-02 22:32:06 +0100

    added water simulation

---
+++ Pi Game/Assets/Scripts/Wave.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    [SerializeField] float amplitude = 1f;
+    [SerializeField] float length = 2f;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float offset = 0f;
+
+    void Update() {
+        offset += Time.deltaTime * speed;
+    }
+
+    public float GetWaveHeight(float x) {
+        return amplitude * Mathf.Sign(x / length + offset);
+    }
+}
