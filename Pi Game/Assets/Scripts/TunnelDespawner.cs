6bc1941faccb6f859cd34341869cb912fb033cc4
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-10-03 10:21:21 +0100

    fixed a bunch of stuff

--- Pi Game/Assets/Scripts/TunnelDespawner.cs
+++ Pi Game/Assets/Scripts/TunnelDespawner.cs
@@ -2,23 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TunnelDespowner : MonoBehaviour
+public class TunnelDespawner : MonoBehaviour
 {
-
     TunnelSpawner tunnelSpawner;
 
-    // Start is called before the first frame update
     void Start()
     {
-        tunnelSpawner = GameObject.FindObjectOfType<TunnelSpawner>();
+        tunnelSpawner = FindObjectOfType<TunnelSpawner>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     //When Exiting a tunnel, destroys it, Spawns another one in the front.
     private void OnTriggerExit(Collider other) 
