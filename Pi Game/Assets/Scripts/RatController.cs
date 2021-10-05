eb33bb9115494cf946472b6a5befccafdb934ea9
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-10-05 20:00:51 +0100

    changed rat movement

--- Pi Game/Assets/Scripts/RatController.cs
+++ Pi Game/Assets/Scripts/RatController.cs
@@ -9,32 +9,37 @@ public class RatController : MonoBehaviour {
     [SerializeField] float jumpForce;
     [SerializeField] LayerMask whatIsGround;
     [HideInInspector] public bool isGrounded;
-    [SerializeField] float gravityStrength = -9.81f;
+    [SerializeField] float gravityStrength = 9.81f;
 
-    Vector3 moveDir;
+    //Vector3 moveDir;
     Vector3 gravity;
     Quaternion lookRot;
     Quaternion startRot;
     Quaternion endRot;
 
-    CameraController cam;
+    //CameraController cam;
     Rigidbody rb;
 
     void Awake() {
         rb = GetComponent<Rigidbody>();
-        cam = FindObjectOfType<CameraController>();
+        //cam = FindObjectOfType<CameraController>();
     }
 
     void Update() {
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
 
-        moveDir = ((ver * cam.forward) + (hor * cam.right)).normalized;
+        //moveDir = ((ver * transform.forward) + (hor * transform.right)).normalized;
+        //moveDir = ver * transform.forward;
+
+        Vector3 pivot = new Vector3(0, 0, transform.position.z);
+        transform.RotateAround(pivot, Vector3.forward, turnSpeed * hor * Time.deltaTime);
+        transform.up = pivot - transform.position;
 
         if (CheckGrounded()) {
             isGrounded = true;
             if (Input.GetKeyDown(KeyCode.Space)) {
-                rb.AddForce(Vector3.up * jumpForce);
+                rb.AddForce(transform.up * jumpForce);
             }
         }
         else {
@@ -48,17 +53,19 @@ public class RatController : MonoBehaviour {
         }
         endRot = Quaternion.Euler(0, lookRot.eulerAngles.y, 0f);
         transform.rotation = Quaternion.Slerp(startRot, endRot, turnSpeed * Time.deltaTime);*/
+        gravity = -transform.up;
+        rb.AddForce(gravity * gravityStrength, ForceMode.Acceleration);
+        //transform.localEulerAngles = Vector3.zero;
     }
 
     void FixedUpdate() {
-        rb.MovePosition(transform.position + moveDir * speed * Time.fixedDeltaTime);
-        rb.AddForce(gravity * gravityStrength * Time.fixedDeltaTime);
+        //rb.MovePosition(transform.position + moveDir * speed * Time.fixedDeltaTime);
     }
 
     bool CheckGrounded() {
         SphereCollider col = GetComponent<SphereCollider>();
         RaycastHit hit;
-        return Physics.SphereCast(transform.position + col.center, col.radius - 0.01f, Vector3.down, out hit, 0.02f, whatIsGround);
+        return Physics.SphereCast(transform.position + col.center, col.radius - 0.01f, -transform.up, out hit, 0.02f, whatIsGround);
         /*
         CapsuleCollider col = GetComponent<CapsuleCollider>();
         Vector3 point1 = transform.position - transform.forward * col.height * 0.5f;
